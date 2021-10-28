857e9332de20a7ce7fae2717a0c25d907c0827c4
Alpha Bah <alphabah@Alphas-MacBook-Pro.local>
2021-10-28 17:16:26 -0600

    first commit

--- script.js
+++ script.js
@@ -1,71 +1,90 @@
 let secn = document.getElementById('secn');
 let min = document.getElementById('min');
 let hourS = document.getElementById('hour');
+const circle = document.querySelector(".progress-circle");
+let circleMnut = document.querySelector(".progress-circleMnut");
+const circleHr = document.querySelector(".progress-circleHr");
+let centrDiv = document.getElementById("innerbar");
+
 
 let hour = 0;
 let minutes = 0;
 let seconds = 0;
 
 
-
+// RUN THE STOPWATCH
 function runTimer() {
-  let tex = document.getElementById("text");
   seconds += 1;
   setnTimer = setTimeout(runTimer, 1000)
   secn.innerHTML = seconds < 10 ? "0" + seconds : seconds;
 
-  let centrDiv = document.getElementById("innerbar");
 
-  let width = seconds * 1.666;
-  if (width >= 100) {
+  // HOURS circle PROGRESS
+  const loadingHr = document.querySelector(".loadingHr");
+  let circumferenceHr = circleHr.getTotalLength();
+  let dataHr = hour * 3.15 - circumferenceHr;
+
+  if (dataHr >= circumferenceHr) {
     clearInterval(setnTimer);
-    rest()
-
+    reset()
   } else {
-    width += 1;
-    centrDiv.style.width = width + "%";
-    tex.innerHTML = seconds < 10 ? "0" + seconds : seconds;
+    dataHr += 1;
+    circleHr.style.strokeDashoffset = dataHr;
+    loadingHr.innerHTML = hour;
+    loadingHr.style.fontSize = 30;
 
   }
 
 
   // MINUTES circle PROGRESS
-  let circleMnut = document.querySelector(".progress-circleMnut");
   let loadingMnut = document.querySelector(".loadingMnut");
   let circumferenceMnut = circleMnut.getTotalLength();
   let dataMnut = minutes * 3.15 - circumferenceMnut;
 
-  console.log(dataMnut)
-
   if (dataMnut >= circumferenceMnut) {
     clearInterval(setnTimer);
-    rest()
-
+    reset()
   } else {
     dataMnut += 1;
     circleMnut.style.strokeDashoffset = dataMnut;
-    loadingMnut.innerHTML = minutes + "Min"
+    loadingMnut.innerHTML = minutes;
+    loadingMnut.style.fontSize = 30;
+
   }
 
+
+
   // SECONDS circle PROGRESS
-  const circle = document.querySelector(".progress-circle");
   const loading = document.querySelector(".loading");
   let circumference = circle.getTotalLength();
   let data = seconds * 3.15 - circumference;
+
   if (data >= circumference) {
     clearInterval(setnTimer);
-    rest()
-
+    reset()
   } else {
     data += 1;
     circle.style.strokeDashoffset = data;
-    loading.innerHTML = seconds + "sec";
-    // console.log("here")
-
+    loading.innerHTML = seconds;
+    loading.style.fontSize = 30;
   }
 
 
 
+  // LINE PROGRESS BAR SECONDS
+  let tex = document.getElementById("text");
+  let width = seconds * 1.655;
+  if (width >= 100) {
+    clearInterval(setnTimer)
+  } else {
+    width += 1;
+    centrDiv.style.width = width + "%";
+    // tex.innerHTML = seconds <10 ? "0"+ seconds:seconds;
+  }
+
+
+
+  // CALCULATING STOP-WATCH
   if (seconds === 60) {
     minutes += 1;
     seconds = 0;
@@ -80,16 +99,21 @@ function runTimer() {
   }
 
 
-
 }
 
+
+// RESET STOP-WATCH
 function reset() {
-  hour += 1;
+  hour = 0;
   minutes = 0;
-  seconds = 0
-  min.innerHTML = "00"
-  hourS.innerHTML = "00";
+  seconds = 0;
+  min.innerHTML = "00:";
+  hourS.innerHTML = "00:";
   secn.innerHTML = "00";
-  
+  circle.style.strokeDashoffset = 189;
+  circleMnut.style.strokeDashoffset = 189;
+  circleHr.style.strokeDashoffset = 189;
+  centrDiv.style.width = 1 + "%";
+
 
 }
